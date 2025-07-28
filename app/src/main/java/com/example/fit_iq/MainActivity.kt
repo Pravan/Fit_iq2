12407d9b188d4e165771bb1fb3513c236124c8a4
Pravan <pravanjanb79@gmail.com>
2025-07-29 00:30:15 +0530

    Add Item Screen

--- app/src/main/java/com/example/fit_iq/MainActivity.kt
+++ app/src/main/java/com/example/fit_iq/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import com.example.fit_iq.ui.presentation.Fit_iqTheme
 import com.example.fit_iq.ui.presentation.add_item.AddItemScreen
 import com.example.fit_iq.ui.presentation.dashboard.Dashboardscreen
 import com.example.fit_iq.ui.presentation.signin.signInScreen
 
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
-class MainActivity : ComponentActivity() {
+ class MainActivity : ComponentActivity() {
 
 
     override fun onCreate(savedInstancesState: Bundle?) {
