a72ef6f740394a1bb3b468739474869ab56d5273
Pravan <pravanjanb79@gmail.com>
2025-08-18 12:55:32 +0530

    firebase and dependency injection

--- app/src/main/java/com/example/fit_iq/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/fit_iq/data/repository/AuthRepositoryImpl.kt
@@ -1,16 +1,23 @@
 package com.example.fit_iq.data.repository
 
 import com.example.fit_iq.domain.model.repository.AuthRepository
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.tasks.await
 
+class AuthRepositoryImpl : AuthRepository {
 
-class AuthRepositoryImpl: AuthRepository {
-
+    val firebaseAuth = FirebaseAuth.getInstance()
 
     override suspend fun signInAnonymously(): Result<Boolean> {
         return try {
-            Result.success(value = true)
-        } catch (e: Exception){
+            firebaseAuth.signInAnonymously().await()
+
+            Result.success(true)
+        } catch (e: Exception) {
+
             Result.failure(e)
         }
     }
 }
+
+
--- app/src/main/java/com/example/fit_iq/domain/model/repository/AuthRepository.kt
+++ app/src/main/java/com/example/fit_iq/domain/model/repository/AuthRepository.kt
@@ -1,5 +1,7 @@
 package com.example.fit_iq.domain.model.repository
 
 interface AuthRepository {
+    abstract val FirebaseAuth: Any
+
     suspend fun signInAnonymously(): Result<Boolean>
 }
